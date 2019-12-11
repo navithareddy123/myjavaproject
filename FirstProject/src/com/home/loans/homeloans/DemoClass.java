290f0a83c38923eb8f07212fdfaedcfd65c6635f
navithareddy123 <navitha.reddy@hotmail.com>
2019-12-11 14:17:00 -0800

    adding demo class to my repository

---
+++ FirstProject/src/com/home/loans/homeloans/DemoClass.java
@@ -0,0 +1,20 @@
+package com.home.loans.homeloans;
+
+public class DemoClass {
+	public void add()
+	{
+		int a=3, b=4,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	DemoClass dc=new DemoClass();
+	dc.add();
+	
+	
+	}
+	
+
+}
