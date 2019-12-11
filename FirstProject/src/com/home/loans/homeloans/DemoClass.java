7d56c53ac3e3608b3ee3c1018682cfaccfe2223b
navithareddy123 <navitha.reddy@hotmail.com>
2019-12-11 14:20:05 -0800

    added sub method to demo class

--- FirstProject/src/com/home/loans/homeloans/DemoClass.java
+++ FirstProject/src/com/home/loans/homeloans/DemoClass.java
@@ -6,12 +6,21 @@ public class DemoClass {
 		int a=3, b=4,c;
 		c=a+b;
 		System.out.println(c);
+		
+	}
+	
+	public void sub()
+	{
+		float x=2.5f, y=5.2f, z;
+		z=x+y;
+		System.out.println(z);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	DemoClass dc=new DemoClass();
 	dc.add();
+	dc.sub();
 	
 	
 	}
